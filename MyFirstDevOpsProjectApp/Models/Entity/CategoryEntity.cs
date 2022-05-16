4962527a18dc72db064297a07d39c12920ee94ee
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-05-16 09:27:05 +0200

    Revert "addedEntities"
    This reverts commit 58deb92935f2b19a61c80d307f20334461f0ed62.

--- MyFirstDevOpsProjectApp/Models/Entity/CategoryEntity.cs
+++ MyFirstDevOpsProjectApp/Models/Entity/CategoryEntity.cs
@@ -1,15 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace MyFirstDevOpsProjectApp.Models.Entity
+﻿namespace MyFirstDevOpsProjectApp.Models.Entity
 {
     public class CategoryEntity
     {
-        [Key]
-        public int Id { get; set; } = 0;
-
-        [Required]
-        [Column(TypeName = "nvarchar(50)")]
-        public string Name { get; set; }
     }
 }
