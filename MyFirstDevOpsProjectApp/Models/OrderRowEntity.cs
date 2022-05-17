31d5639aa7c62e26cee51438aade4c77d2623eb5
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-05-17 10:47:41 +0200

    AddEntities- AddedOrderRowEntity

---
+++ MyFirstDevOpsProjectApp/Models/OrderRowEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFirstDevOpsProjectApp.Models
+{
+    public class OrderRowEntity
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+
+        [Required]
+        [Column(TypeName = "Int")]
+        public int Quantity { get; set; }
+        [Required]
+        [Column(TypeName = "Decimal")]
+        public decimal TotalProductPrice { get; set; }
+
+        public OrderEntity OrderId { get; set; }
+    }
+}
