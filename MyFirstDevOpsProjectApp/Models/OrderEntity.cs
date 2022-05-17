2a4eed5a59092966dca936de9c263638fbd4b441
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-05-17 10:29:14 +0200

    AddEntities- AddedOrderEntity

---
+++ MyFirstDevOpsProjectApp/Models/OrderEntity.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFirstDevOpsProjectApp.Models
+{
+    public class OrderEntity
+    {
+        public int Id { get; set; } = 0;
+
+
+        public CustomerEntity CustomerId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public OrderStatus Order { get; set; }
+        [Required]
+        [Column(TypeName = "DateTime")]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [Column(TypeName = "DateTime")]
+        public DateTime DeliverDate { get; set; }
+        [Required]
+        [Column(TypeName = "Decimal")]
+        public decimal ShippingFee { get; set; }
+        [Required]
+        [Column(TypeName = "Decimal")]
+        public decimal TotalPrice { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+
+        Onhold,
+        Initialized,
+        Delivered,
+        Finished
+
+
+    }
+}
