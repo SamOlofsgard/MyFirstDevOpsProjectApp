29edbaebc86a3c5c0d9001617eaa83078ad348e2
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-05-17 10:14:23 +0200

    AddEntities- AddedCustomerEntity

---
+++ MyFirstDevOpsProjectApp/Models/CustomerEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFirstDevOpsProjectApp.Models
+{
+    public class CustomerEntity
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string StreetName { get; set; } = "";
+
+        [Column(TypeName = "char(6)")]
+        public string PostalCode { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string City { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Country { get; set; } = "";
+    }
+}
